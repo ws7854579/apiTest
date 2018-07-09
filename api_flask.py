6ea445b5cb54658218c6334906f98c9a1761312f
sunsuwei <790708124@qq.com>
2018-07-09 18:31:45 +0800

    修改了mock方法

--- api_flask.py
+++ api_flask.py
@@ -34,7 +34,10 @@ def get_url(path):
     list_req_data = []
     list_req_blob = []
     mylogger.info('url：%s'%path)
+    len_mock = len(mock_list_all)
+    count = 0
     for api in mock_list_all:
+        count += 1
         mylogger.info(api)
         mylogger.info(api['url'])
         if api['url'] == path:
@@ -42,28 +45,41 @@ def get_url(path):
             list_req_form.append(api['req_form'])
             list_req_data.append(api['req_data'])
             list_req_blob.append(api['req_blob'])
+        elif count == len_mock:
+            return '没有找到相匹配的url'
+        mylogger.info(count)
     #暂时用‘=’分割req_data
     mylogger.info(list_req_data)
     mylogger.info(list_req_blob)
     if request.method == 'GET':
         mylogger.info('method==GET')
+        count_1 = 0
         for req in list_req_data:
+            len_req_data = len(list_req_data)
+            count_1 += 1
             par = req.split('=')[0]
             par_data = req.split('=')[1]
             url_req_data = request.args.get(par)
             mylogger.info(u'获取请求参数:%s'%url_req_data)
+            mylogger.info(u'获取数据库中存储的请求参数：%s'%par_data)
             if url_req_data == par_data:
                 mylogger.info(u'在库中找到了相同的参数')
-                for api in mock_list_all:
-                    if url_req_data in api['req_data']:
+                for mock_info in mock_list_all:
+                    mylogger.info(mock_info)
+                    mylogger.info('req_data:%s' % mock_info['req_data'])
+                    if str(url_req_data) in str(mock_info['req_data']):
                         #json = {"resCode":"2010","resMsg":"身份验证成功","tid":"1035005015310118542372260","sign":"A74D2BFC07EFF29A597897E0BA48D427"}
                         #return jsonify(json)
-                        text_blob = json.loads(api['req_blob'])
+                        mylogger.info('url中请求的参数在库中找到了匹配项')
+                        text_blob = json.loads(mock_info['req_blob'])
                         return jsonify(text_blob)
                     else:
-                        return u'没有匹配的参数'
+                        mylogger.info('与参数数据相匹配的api不是这个，继续查找下一个')
+            elif count_1 == len_req_data:
+                return '参数没找到相匹配的'
             else:
-                return u'没有匹配的字段'
+                mylogger.info('参数继续找下一个')
+
     else:
         return u'还没写post'
 
